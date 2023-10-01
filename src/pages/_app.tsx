cf93eabf812d00d9acd415c735ffa40c4ad175c4
ThiagoLG <thiago.lg53@gmail.com>
2023-10-01 11:15:15 -0300

    💄 style: setup stitches config and global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { globalStyles } from '@/styles/global';
 import type { AppProps } from 'next/app'
+
+globalStyles();
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: "$green500",
+  borderRadius: 8
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>Enviar</Button>
   )
 }
