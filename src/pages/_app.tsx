0c670ba6a1d11da94037c2ecd8a431168c464ca6
ThiagoLG <thiago.lg53@gmail.com>
2023-10-01 11:58:16 -0300

    ✨ feat: Created header 'component'

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import { globalStyles } from '@/styles/global';
+import { Container, Header } from '@/styles/pages/app';
 import type { AppProps } from 'next/app'
+import Image from 'next/image';
+import logoImg from '../assets/logo.svg'
 
 globalStyles();
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="header logo" width={130} height={52} />
+      </Header>
+
+      <Component {...pageProps} />
+
+    </Container>
+  )
 }
 
 export default App;
